8cc71278a1ffe88787bd7bea24d08bff55fefd16
Anlon <HullBend@users.noreply.github.com>
2018-02-01 21:48:27 +0100

    initial

---
+++ src/main/cpp/stdafx.cpp
@@ -0,0 +1,20 @@
+/* ------------------------------------------------------------------------ */
+/* stdafx.cpp :                                                             */
+/* Source file that includes just the standard includes                     */
+/*                                                                          */
+/* For Windows:                                                             */
+/*    jdcesrv.pch is the pre-compiled header                                */
+/*    stdafx.obj will contain the pre-compiled type information             */
+/* ------------------------------------------------------------------------ */
+
+
+
+#include "stdafx.h"
+
+
+
+/* ------------------------------------------------------------------------ */
+/* NOTE:                                                                    */
+/* Reference any additional headers you need in stdafx.h, not in this file! */
+/* ------------------------------------------------------------------------ */
+
